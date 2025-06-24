220c8a9165e8a992fc1a1f7438ed473bc67471fc
Philipp Hofmann <phofmann200@gmail.com>
2025-06-24 16:40:24 +0200

    finished ex00

---
+++ ex00/megaphone.cpp
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: phhofman <phhofman@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/06/24 10:50:01 by phhofman          #+#    #+#             */
+/*   Updated: 2025/06/24 16:37:15 by phhofman         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+
+int	main(int argc, char **argv)
+{
+	if (argc == 1)
+	{
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+		return 0;
+	}
+	for (int i = 1; i < argc; i++)
+		for (size_t j = 0; j < std::strlen(argv[i]); j++)
+			std::cout << (char)std::toupper(argv[i][j]);
+	std::cout << std::endl;
+	return 0;
+}
