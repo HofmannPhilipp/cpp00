8106304f37c7bd4a5c8ac69292fc583051c4f9af
Philipp Hofmann <phofmann200@gmail.com>
2025-07-02 18:48:23 +0200

    add displayStatus _displayTimestamp

--- ex00/ex02/Account.cpp
+++ ex00/ex02/Account.cpp
@@ -6,16 +6,33 @@
 /*   By: phhofman <phhofman@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/02 17:30:41 by phhofman          #+#    #+#             */
-/*   Updated: 2025/07/02 17:53:19 by phhofman         ###   ########.fr       */
+/*   Updated: 2025/07/02 18:46:41 by phhofman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <Account.hpp>
+#include <iostream>
+#include <ctime>
+#include <iomanip>
 
 int	Account::_nbAccounts = 0;
 int	Account::_totalAmount = 0;
 int	Account::_totalNbDeposits = 0;
 int	Account::_totalNbWithdrawals = 0;
+
+Account::Account( int initial_deposit ) {
+	this->_accountIndex = Account::_nbAccounts;
+	Account::_nbAccounts++;
+	Account::_totalAmount += initial_deposit;
+}
+
+Account::~Account( void ) {
+	
+}
+
+void	makeDeposit( int deposit ) {
+	this.
+}
 
 int	Account::getNbAccounts( void ) {
 	return Account::_nbAccounts;
@@ -35,4 +52,27 @@ void	Account::displayAccountsInfos( void ) {
 	std::cout << "total:" << Account::getNbAccounts << ";";
 	std::cout << "deposits:" << Account::getNbAccounts << ";";
 	std::cout << "withdrawals:" << Account::getNbAccounts << std::endl;
+}
+
+void	Account::_displayTimestamp( void ) {
+	std::time_t	now = std::time(0);
+	std::tm*	localTime = std::localtime(&now);
+
+	std::cout
+		<< "["
+		<< (1900 + localTime->tm_year)
+		<< std::setw(2) << std::setfill('0') << (1 + localTime->tm_mon)
+		<< std::setw(2) << std::setfill('0') << localTime->tm_mday
+		<< "_"
+		<< std::setw(2) << std::setfill('0') << localTime->tm_hour
+		<< std::setw(2) << std::setfill('0') << localTime->tm_min
+		<< std::setw(2) << std::setfill('0') << localTime->tm_sec
+		<< "] ";
+}
+
+void	Account::displayStatus( void ) const{
+	Account::_displayTimestamp();
+	std::cout << "index:" << this->_accountIndex << ";";
+	std::cout << "amount:" << this->_amount << ";";
+	std::cout << "created" << std::endl;
 }
